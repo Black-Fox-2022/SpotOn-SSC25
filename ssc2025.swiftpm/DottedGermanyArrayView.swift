34bda87802bbe82e4c78723d2c67d0d124df00fe
Blacky <106263486+Black-Fox-2022@users.noreply.github.com>
2025-01-29 17:42:32 +0100

    New app

---
+++ ssc2025.swiftpm/DottedGermanyArrayView.swift
@@ -0,0 +1,145 @@
+//
+//  DottedGermanyArrayView.swift
+//  ssc2025
+//
+//  Created by Lukas on 29.01.25.
+//
+
+
+import SwiftUI
+
+struct DottedGermanyArrayView: View {
+
+    let germanyMatrix: [[Int]] = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0], // 5
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0], // 10
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], // 15
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0], // 20
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], // 25
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0], // 30
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 35
+    ]
+
+    let dotSize: CGFloat = 10
+    let spacing: CGFloat = 6
+
+    @State private var activePoint: (row: Int, col: Int)? = nil
+    @State private var fadingPoint: (row: Int, col: Int)? = nil
+
+
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading){
+                Text("In germany on average")
+                Text("every 7 seconds")
+                    .foregroundStyle(Color(.orange))
+                Text("a firebrigarde is being called to an emergency")
+            }
+            .font(.system(size: 60, weight: .regular))
+            .padding()
+
+
+            VStack(spacing: spacing) {
+                ForEach(0..<germanyMatrix.count, id: \.self) { row in
+                    HStack(spacing: spacing) {
+                        ForEach(0..<germanyMatrix[row].count, id: \.self) { col in
+                            if germanyMatrix[row][col] == 1 {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundColor(
+                                        (activePoint?.row == row && activePoint?.col == col) ? Color(.red) :
+                                        (fadingPoint?.row == row && fadingPoint?.col == col) ? Color(.red).opacity(0.5) :
+                                        .primary.opacity(0.5)
+                                    )
+                                    .frame(width: dotSize, height: dotSize)
+                                    .animation(.spring(duration: 0.7), value: activePoint != nil)
+                                    .animation(.easeOut(duration: 0.4), value: fadingPoint != nil)
+                            } else {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundColor(.clear)
+                                    .frame(width: dotSize, height: dotSize)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 35)
+        .onAppear {
+            startFlashingMode()
+        }
+    }
+
+    func startFlashingMode() {
+        runDot()
+        Timer.scheduledTimer(withTimeInterval: Double.random(in: 6...8), repeats: true) { _ in
+            DispatchQueue.main.async {
+                runDot()
+            }
+        }
+    }
+
+    func runDot() {
+        print("start New Flash")
+        DispatchQueue.main.async {
+            var possiblePoints: [(Int, Int)] = []
+            for row in 0..<germanyMatrix.count {
+                for col in 0..<germanyMatrix[row].count {
+                    if germanyMatrix[row][col] == 1 {
+                        possiblePoints.append((row, col))
+                    }
+                }
+            }
+
+            if let newPoint = possiblePoints.randomElement() {
+                activePoint = newPoint
+                print("new Point")
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    print("switching to Fading Point")
+                    fadingPoint = newPoint
+                    activePoint = nil
+
+                    let fadeDuration = Double.random(in: 12.5...22.5)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + fadeDuration) {
+                        print("done")
+                        fadingPoint = nil
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DottedGermanyArrayView_Previews: PreviewProvider {
+    static var previews: some View {
+        DottedGermanyArrayView()
+    }
+}
